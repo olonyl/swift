31ac3c6ab3b042bc3a47860431647da92fb1c20d
olonyl <jose87.landeros@gmail.com>
2019-09-02 15:47:54 -0600

    first commit

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,24 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView : WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType:"html"){
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+            
+        }
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
